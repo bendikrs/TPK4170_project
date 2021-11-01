7ed9151b0729cc4da711720bfe948dee9badc004
bendikrs <bendik_sto@hotmail.com>
2021-11-01 13:34:40 +0100

    task3 visualisering

--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,7 @@
 import sympy as sp
+from modern_robotics import FKinSpace, Adjoint, se3ToVec, MatrixLog6, TransInv, JacobianSpace
+import numpy as np
+
 
 
 def exp6(twist, theta):
@@ -30,3 +33,71 @@ def Ad(T):
     AdT[3:, 3:] = R
     AdT[3:, :3] = skew(T[:3, 3]) * R
     return AdT
+
+
+def IKinSpace_our(Slist, M, T, thetalist0, eomg, ev):
+    """Computes inverse kinematics in the space frame for an open chain robot
+
+    :param Slist: The joint screw axes in the space frame when the
+                  manipulator is at the home position, in the format of a
+                  matrix with axes as the columns
+    :param M: The home configuration of the end-effector
+    :param T: The desired end-effector configuration Tsd
+    :param thetalist0: An initial guess of joint angles that are close to
+                       satisfying Tsd
+    :param eomg: A small positive tolerance on the end-effector orientation
+                 error. The returned joint angles must give an end-effector
+                 orientation error less than eomg
+    :param ev: A small positive tolerance on the end-effector linear position
+               error. The returned joint angles must give an end-effector
+               position error less than ev
+    :return thetalist: Joint angles that achieve T within the specified
+                       tolerances,
+    :return success: A logical value where TRUE means that the function found
+                     a solution and FALSE means that it ran through the set
+                     number of maximum iterations without finding a solution
+                     within the tolerances eomg and ev.
+    Uses an iterative Newton-Raphson root-finding method.
+    The maximum number of iterations before the algorithm is terminated has
+    been hardcoded in as a variable called maxiterations. It is set to 20 at
+    the start of the function, but can be changed if needed.
+
+    Example Input:
+        Slist = np.array([[0, 0,  1,  4, 0,    0],
+                          [0, 0,  0,  0, 1,    0],
+                          [0, 0, -1, -6, 0, -0.1]]).T
+        M = np.array([[-1, 0,  0, 0],
+                      [ 0, 1,  0, 6],
+                      [ 0, 0, -1, 2],
+                      [ 0, 0,  0, 1]])
+        T = np.array([[0, 1,  0,     -5],
+                      [1, 0,  0,      4],
+                      [0, 0, -1, 1.6858],
+                      [0, 0,  0,      1]])
+        thetalist0 = np.array([1.5, 2.5, 3])
+        eomg = 0.01
+        ev = 0.001
+    Output:
+        (np.array([ 1.57073783,  2.99966384,  3.1415342 ]), True)
+    """
+
+    Tsb = []
+    thetalist = np.array(thetalist0).copy()
+    i = 0
+    maxiterations = 20
+    Tsb.append(FKinSpace(M,Slist, thetalist))
+    Vs = np.dot(Adjoint(Tsb[0]), \
+                se3ToVec(MatrixLog6(np.dot(TransInv(Tsb[0]), T))))
+    err = np.linalg.norm([Vs[0], Vs[1], Vs[2]]) > eomg \
+          or np.linalg.norm([Vs[3], Vs[4], Vs[5]]) > ev
+    while err and i < maxiterations:
+        thetalist = thetalist \
+                    + np.dot(np.linalg.pinv(JacobianSpace(Slist, \
+                                                          thetalist)), Vs)
+        i = i + 1
+        Tsb.append(FKinSpace(M, Slist, thetalist))
+        Vs = np.dot(Adjoint(Tsb[-1]), \
+                    se3ToVec(MatrixLog6(np.dot(TransInv(Tsb[-1]), T))))
+        err = np.linalg.norm([Vs[0], Vs[1], Vs[2]]) > eomg \
+              or np.linalg.norm([Vs[3], Vs[4], Vs[5]]) > ev
+    return (thetalist, not err, Tsb, i)
