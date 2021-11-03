36d2f36929f76cad2331905cf952530c49571188
bendikrs <bendik_sto@hotmail.com>
2021-11-03 12:02:35 +0100

    div rydding

--- src/kr6.py
+++ src/kr6.py
@@ -1,5 +1,8 @@
 import numpy as np
 import modern_robotics as mr
+from tpk4170.visualization import Viewer
+from src.dh import DHFrame
+import src.tools as tools
 
 if __name__ == "__main__":
     from dh import DHLink
@@ -12,7 +15,7 @@ PI = np.pi
 class KR6:
 
 
-    def __init__(self, theta = np.array([0,0,0,0,0,0])):
+    def __init__(self, theta = np.array([0,0,0,0,0,0]), thetalist0 = [0,0,0,0,0,0], eomg=0.01, ev=0.01):
 
         self.d1, self.d2, self.d3, self.d4 = 0.400, 0.455, 0.420, 0.080 #distansene mellom jointsene som ikke er 0
         self.a1, self.a2 = 0.025, 0.035 #offsetsene 
@@ -23,9 +26,7 @@ class KR6:
                 [0, -1,  0,                               0],
                 [1,  0,  0,                 self.a2+self.d1],
                 [0,  0,  0,                               1]
-            ]
-        )
-
+            ])
         self._Slist = np.array(
             [
                 [0, 0,-1,                  0,                 0,                       0],
@@ -34,8 +35,7 @@ class KR6:
                 [-1,0, 0,                  0,-(self.a2+self.d1),                       0],
                 [0, 1, 0, -(self.d1+self.a2),                 0, self.a1+self.d2+self.d3],
                 [-1,0, 0,                  0,-(self.a2+self.d1),                       0]
-            ]
-        ).T
+            ]).T
 
         self.theta = np.atleast_2d(theta).T
         
@@ -47,7 +47,7 @@ class KR6:
                                 [0],        # from joint 5 to joint 6
                                 [0]])       # from joint 6 to body frame
         
-        for i in range(1, len(self.thetaDH0)):
+        for i in range(1, len(self.thetaDH0)): # Add the theta values to the DH configuration
             self.thetaDH0[i] += self.theta[i-1]
 
         self.alpha = np.array([[PI],
@@ -82,14 +82,26 @@ class KR6:
         for i in range(len(self.dhList)):
             self.linkList.append(DHLink(*self.dhList[i]))
 
-        self.fk_dh_zero = self.linkList[0].matrix() # DH zero configuration
+        self.Tsb_dh = self.linkList[0].matrix() # DH zero configuration
         self.Tlist = [self.linkList[0].matrix()]    # Contains all the transformation matrices
-
         for i in range(1, len(self.dhList)):
-            self.fk_dh_zero = self.fk_dh_zero @ self.linkList[i].matrix()
-            self.Tlist.append(self.Tlist[i-1] @ self.linkList[i].matrix())
+            self.Tsb_dh = self.Tsb_dh @ self.linkList[i].matrix() # T06
+            self.Tlist.append(self.Tlist[i-1] @ self.linkList[i].matrix()) # [T01, T02, T03, T04, T05, T06]
             
 
-        self.T_sb_poe = mr.FKinSpace(self._M, self._Slist, self.theta)
+        self.Tsb_poe = mr.FKinSpace(self._M, self._Slist, self.theta)
 
+        self.IK = tools.IKinSpace_our(self._Slist, self._M, self.Tsb_dh, thetalist0, eomg, ev)
+        self.Tsb_IK = self.IK[2][-1]
+
+    def add_FK_to_viewer(self, viewer: Viewer):
+        for i in range(len(self.Tlist)):
+            viewer.add(DHFrame(self.Tlist[i]))
+
+    def add_aIK_to_viewer(self, viewer: Viewer): # Analytical IK
+        pass
+
+    def add_nIK_to_viewer(self, viewer: Viewer): # Numerical IK
+        kr6_temp = KR6(self.IK[0])
+        kr6_temp.add_FK_to_viewer(viewer)
 
