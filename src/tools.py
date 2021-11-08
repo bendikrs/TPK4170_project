e850c6ac1486400671f1f9ab0b90f2d6824254d4
jakoboss69 <jakob_i_m@hotmail.com>
2021-11-08 13:42:48 +0100

    Lagd inversekinematicstheta123 i tools

--- src/tools.py
+++ src/tools.py
@@ -125,3 +125,59 @@ def IKinSpace_our(Slist, M, T, thetalist0, eomg, ev):
         err = np.linalg.norm([Vs[0], Vs[1], Vs[2]]) > eomg \
               or np.linalg.norm([Vs[3], Vs[4], Vs[5]]) > ev
     return (thetalist, not err, Tsb, i)
+
+
+def pointFromT(T=np.array) -> np.array:
+    P = np.zeros(3)
+    for i in range(len(T)):
+        P[i:] = T[i][3]
+    return P
+
+
+
+def makeT_SW(T_SB=np.array) -> np.array:
+    T_BW = np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, -0.080],
+                     [0, 0, 0, 1]])
+    return np.dot(T_SB, T_BW)
+
+
+def inverseKinematicsTheta123(T_SB:np.array) -> np.array: #returnerer liste med 4 alternative konfig for theta 1,2 og 3
+    r1, r2, r3, r4, a1, a2 = 0, 0.400, 0.455, 10**(-3)*np.sqrt(420**2+35**2), 0.025, 0.035
+    T_SW = makeT_SW(T_SB)
+    P_W = pointFromT(T_SW)
+    
+
+    theta1_i = np.arctan2(-P_W[1], P_W[0])
+    theta1_ii = np.arctan2(P_W[1], -P_W[0])
+
+    Pz, Px, Py = P_W[2]-r2, \
+                 P_W[0] - np.cos(theta1_i) * a1, \
+                 P_W[1]+np.sin(theta1_ii) * a1
+    c3 = (Pz**2 + Px**2 + Py**2 - r3**2 - r4**2) / (2 * r3 * r4)
+
+    theta3_i = np.arccos(c3)
+    theta3_ii = - theta3_i
+
+    s3_pos = np.sqrt(1-c3**2)
+    s3_neg = -np.sqrt(1-c3**2)
+
+    c2_poss3 = (np.sqrt(Px**2 + Py**2) * (r3+r4*c3) +
+            Pz * r4 * s3_pos)/(r3**2+r4**2+2*r3*r4*c3)
+
+    c2_negs3 = (np.sqrt(Px**2 + Py**2) * (r3+r4*c3) +
+            Pz * r4 * s3_neg)/(r3**2+r4**2+2*r3*r4*c3)
+
+    theta2_i = np.arccos(c2_poss3)
+    theta2_ii = np.arccos(-c2_poss3)
+    theta2_iii = np.arccos(c2_negs3)
+    theta2_iv = np.arccos(-c2_negs3)
+
+    alt1 = [theta1_i, theta2_i , theta3_i - np.arctan(35/420)]
+    alt2 = [theta1_i, theta2_iii , theta3_ii - np.arctan(35/420)]
+    alt3 = [theta1_ii, theta2_ii , theta3_i + np.arctan(35/420)]
+    alt4 = [theta1_ii, theta2_iv , theta3_ii + np.arctan(35/420)]
+
+
+    return np.array([alt1,alt2,alt3,alt4])
