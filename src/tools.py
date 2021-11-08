ce6e2c5655ce90d71294e27b8d0325d08f8a4dee
bendikrs <bendik_sto@hotmail.com>
2021-11-08 17:25:16 +0100

    div rydding, samt task 2 comparison

--- src/tools.py
+++ src/tools.py
@@ -60,49 +60,11 @@ def Ad(T):
 
 
 def IKinSpace_our(Slist, M, T, thetalist0, eomg, ev):
-    """Computes inverse kinematics in the space frame for an open chain robot
-
-    :param Slist: The joint screw axes in the space frame when the
-                  manipulator is at the home position, in the format of a
-                  matrix with axes as the columns
-    :param M: The home configuration of the end-effector
-    :param T: The desired end-effector configuration Tsd
-    :param thetalist0: An initial guess of joint angles that are close to
-                       satisfying Tsd
-    :param eomg: A small positive tolerance on the end-effector orientation
-                 error. The returned joint angles must give an end-effector
-                 orientation error less than eomg
-    :param ev: A small positive tolerance on the end-effector linear position
-               error. The returned joint angles must give an end-effector
-               position error less than ev
-    :return thetalist: Joint angles that achieve T within the specified
-                       tolerances,
-    :return success: A logical value where TRUE means that the function found
-                     a solution and FALSE means that it ran through the set
-                     number of maximum iterations without finding a solution
-                     within the tolerances eomg and ev.
-    Uses an iterative Newton-Raphson root-finding method.
-    The maximum number of iterations before the algorithm is terminated has
-    been hardcoded in as a variable called maxiterations. It is set to 20 at
-    the start of the function, but can be changed if needed.
-
-    Example Input:
-        Slist = np.array([[0, 0,  1,  4, 0,    0],
-                          [0, 0,  0,  0, 1,    0],
-                          [0, 0, -1, -6, 0, -0.1]]).T
-        M = np.array([[-1, 0,  0, 0],
-                      [ 0, 1,  0, 6],
-                      [ 0, 0, -1, 2],
-                      [ 0, 0,  0, 1]])
-        T = np.array([[0, 1,  0,     -5],
-                      [1, 0,  0,      4],
-                      [0, 0, -1, 1.6858],
-                      [0, 0,  0,      1]])
-        thetalist0 = np.array([1.5, 2.5, 3])
-        eomg = 0.01
-        ev = 0.001
-    Output:
-        (np.array([ 1.57073783,  2.99966384,  3.1415342 ]), True)
+    """This function borrowed from the IKinSpace() function from the Modern Robotics library, and then modified.
+        The function does now return a list of all the Tsb's, instead of just the last one.
+        It also returns i, which is the number of iterations it took to find the solution.
+    
+    Computes inverse kinematics in the space frame for an open chain robot
     """
 
     Tsb = []
